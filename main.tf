9ac4b81774b75bbca909da6a5bddd55ecc20a796
obasic <orestjoe@gmail.com>
2023-07-06 21:21:21 +0000

    saving state in bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 module "gke_cluster" {
-  source         = "github.com/Chubasic/tf-gke"
+  source         = "github.com/Chubasic/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
+
+terraform {
+    backend "gcs" {
+    #   bucket = var.GC_STORAGE
+      bucket = "tf-kata01-b"
+      prefix = "terraform/state"
+    }
+  }
+
